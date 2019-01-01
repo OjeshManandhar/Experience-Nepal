fdfadfe11a0a9124d10116c8241cbc730e182841
OjeshManandhar <ozes.manandhar@gmail.com>
2019-01-01 11:16:40 +0545

    Initial commit

---
+++ map/map.js
@@ -0,0 +1,37 @@
+L.mapbox.accessToken = token;
+
+const map = L.mapbox.map('map', 'mapbox.streets');
+const geolocate = document.getElementById('geolocate');
+const myLayer = L.mapbox.featureLayer().addTo(map);
+
+map.on('load', function(e) {
+    map.locate();
+});
+
+map.on('locationfound', function(e) {
+    map.fitBounds(e.bounds);
+
+    map.setView([e.latlng.lat, e.latlng.lng], 10);
+
+    myLayer.setGeoJSON({
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [e.latlng.lng, e.latlng.lat]
+        },
+        properties: {
+            'title': 'You are here',
+            'marker-color': '#ff8888',
+            'marker-size': 'large',
+            'marker-symbol': 'star'
+        }
+    });
+});
+
+map.on('locationerror', function() {
+    console.log('Position could not be found');
+});
+
+map.featureLayer.setGeoJSON(geojson);
+
+L.control.locate().addTo(map);
