a30497370a907f99d730a5fabf07239db260632e
OjeshManandhar <ozes.manandhar@gmail.com>
2019-01-26 02:51:39 +0545

    Plot locations

--- map/test.js
+++ map/test.js
@@ -6,3 +6,21 @@ var map = new mapboxgl.Map({
     center: [84, 28],
     zoom: 6.25
 });
+
+map.on('load', function(e) {
+    // Add the data to your map as a layer
+    map.addLayer({
+        id: 'locations',
+        type: 'symbol',
+        // Add a GeoJSON source containing place coordinates and information.
+        source: {
+            type: 'geojson',
+            data: geojson
+        },
+        layout: {
+            'icon-image': '{marker-symbol}-15',
+            'icon-allow-overlap': true,
+            'icon-size': 1.5
+        }
+    });
+});
