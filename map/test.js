6e19598a336188f03238dc1abc60ed8bf21bd2fe
OjeshManandhar <ozes.manandhar@gmail.com>
2019-01-26 02:35:50 +0545

    Display map of Nepal on Load

--- map/test.js
+++ map/test.js
@@ -3,6 +3,6 @@ mapboxgl.accessToken = token;
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v9',
-    center: [-74.50, 40],
-    zoom: 9
+    center: [84, 28],
+    zoom: 6.25
 });
