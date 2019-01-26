b5b7dd581da11043fe56eefcee7356b5e06bf171
OjeshManandhar <ozes.manandhar@gmail.com>
2019-01-26 14:07:33 +0545

    Add listing

--- map/test.js
+++ map/test.js
@@ -1,5 +1,34 @@
 mapboxgl.accessToken = token;
 
+function buildLocationList(data) {
+    // Iterate through the list of stores
+    for (i = 0; i < data.features.length; i++) {
+        var currentFeature = data.features[i];
+        // Shorten data.feature.properties to just `prop` so we're not
+        // writing this long form over and over again.
+        var prop = currentFeature.properties;
+        // Select the listing container in the HTML and append a div
+        // with the class 'item' for each store
+        var listings = document.getElementById('listings');
+        var listing = listings.appendChild(document.createElement('div'));
+        listing.className = 'item';
+        listing.id = 'listing-' + i;
+
+        // Create a new link with the class 'title' for each store
+        // and fill it with the store address
+        var link = listing.appendChild(document.createElement('a'));
+        link.href = '#';
+        link.className = 'title';
+        link.dataPosition = i;
+        link.innerHTML = prop.title;
+
+        // Create a new div with the class 'details' for each store
+        // and fill it with the city and phone number
+        var details = listing.appendChild(document.createElement('div'));
+        details.innerHTML = prop.description;
+    }
+}
+
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v9',
@@ -23,6 +52,8 @@ map.on('load', function(e) {
             'icon-size': 1.5
         }
     });
+
+    buildLocationList(geojson);
 });
 
 //Show Navigation Controls i.e. Zoom In, Zoom Out and Comaps
